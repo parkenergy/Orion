4765219e57f46ce0c569fb1f4ef74f21bd868c3b
Cale Kennedy <me@calekennedy.com>
2015-07-16 16:23:36 -0500

    updated common

--- _common_packaged/models/user.js
+++ _common_packaged/models/user.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.ObjectId;
 var autopopulate = require('mongoose-autopopulate');
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
 
 /* Declaration
@@ -34,30 +34,31 @@ UserSchema.plugin(autopopulate);
 
 UserSchema.pre('save', function(next) {
   var user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-      if (err) return next(err);
-
-      // hash the password along with our new salt
-      bcrypt.hash(user.password, salt, function(err, hash) {
-          if (err) return next(err);
-
-          // override the cleartext password with the hashed one
-          user.password = hash;
-          next();
-      });
-  });
+  return next();
+  // // only hash the password if it has been modified (or is new)
+  // if (!user.isModified('password')) return next();
+  //
+  // // generate a salt
+  // bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  //     if (err) return next(err);
+  //
+  //     // hash the password along with our new salt
+  //     bcrypt.hash(user.password, salt, function(err, hash) {
+  //         if (err) return next(err);
+  //
+  //         // override the cleartext password with the hashed one
+  //         user.password = hash;
+  //         next();
+  //     });
+  // });
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-      if (err) return cb(err);
-      cb(null, isMatch);
-  });
+  return cb(null, null);
+  // bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+  //     if (err) return cb(err);
+  //     cb(null, isMatch);
+  // });
 };
 
 /* Virtual Fields
