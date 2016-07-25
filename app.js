a7c91a189b4ed4b28bdf5b6f94ce8a30aa9d24af
tsturzl <travissturzl@gmail.com>
2016-07-25 12:44:55 -0600

    Lookup tech by tech name rather than techId

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(sessions({
   duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
   activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 }));
-log.middleware(app);
+//log.middleware(app);
 
 
 //Load custom middleware
