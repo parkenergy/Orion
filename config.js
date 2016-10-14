1562bff4729aa7f3650746827f30e9d4cb94a75d
tsturzl <travissturzl@gmail.com>
2016-10-14 12:17:14 -0600

    add log

--- config.js
+++ config.js
@@ -1,26 +1,64 @@
+var BunyanSlack = require('bunyan-slack');
+
 var config = {
   production: {
     mongodb: "mongodb://172.31.8.0:27017/orion",
     viewsPath: "./lib/views",
-    logLevel: "info",
+    logging: {
+      name: "Orion",
+      streams: [
+        {
+          stream: process.stdout,
+          level: 'info'
+        },
+        {
+          stream: new BunyanSlack({
+            webhook_url: "https://hooks.slack.com/services/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",
+            channel: 'alerts',
+            username: 'Orion'
+          }),
+          level: 'error'
+        }
+      ]
+    },
     port: 3000
   },
   staging: {
     mongodb: "mongodb://localhost/orion-stage",
     viewsPath: "./lib/views",
-    logLevel: "debug",
+    logging: {
+      name: "Orion",
+      streams: [{
+        stream: process.stdout,
+        level: 'debug'
+      }]
+    },
     port: 3000
   },
   development: {
     mongodb: "mongodb://admin:password@ds043082.mongolab.com:43082/heroku_app37485205",
     viewsPath: "./lib/views",
-    logLevel: "trace",
+    logging: {
+      name: "Orion",
+      streams: [
+        {
+          stream: process.stdout,
+          level: 'trace'
+        }
+      ]
+    },
     port: 3000
   },
   test: {
     mongodb: "mongodb://localhost/orion-test",
     viewsPath: "./lib/views",
-    logLevel: "warn",
+    logging: {
+      name: "Orion",
+      streams: [{
+        stream: process.stdout,
+        level: 'warn'
+      }]
+    },
     port: 3000
   }
 };
