de7cf8c65b5d52226e7aa30e9cd55408258c679f
tsturzl <travissturzl@gmail.com>
2016-11-03 15:45:51 -0600

    syncDB script

--- syncDevDB.sh
+++ syncDevDB.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mongo localhost:27017/orion-dev --eval 'db.dropDatabase()'
+
 ssh -f -o ExitOnForwardFailure=yes -L 27037:localhost:27017 access@54.187.140.31 sleep 900
 
 mongo --eval 'db.copyDatabase("orion", "orion-dev", "localhost:27037")'
+
+killall ssh
