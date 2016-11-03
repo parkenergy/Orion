cb171675a93c42c5a48b18a50eb1c88dbdd82f8b
tsturzl <travissturzl@gmail.com>
2016-11-03 15:28:35 -0600

    use local db for dev

--- syncDevDB.sh
+++ syncDevDB.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ssh -f -o ExitOnForwardFailure=yes -L 27027:localhost:27017 access@54.187.140.31 sleep 10
+ssh -f -o ExitOnForwardFailure=yes -L 27027:localhost:27017 access@54.187.140.31 sleep 900
 
 mongo --eval 'db.copyDatabase("orion", "orion-dev", "localhost:27027")'
