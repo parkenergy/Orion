79b198be5e8c9e986e5c82c29530f1570b5729e6
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-01-19 09:29:57 -0600

    Unit tests for Part Order #UpdateDoc completed

--- test/unit/callReport.js
+++ test/unit/callReport.js
@@ -14,11 +14,11 @@ const should     = require('should');
 const _          = require('lodash');
 const fixture    = require('../fixture/callReport.json');
 
-before((done) => {
+before(done => {
   CallReport.remove({}, done);
 });
 
-after((done) => {
+after(done => {
   CallReport.remove({}, done);
 });
 
@@ -28,7 +28,7 @@ describe("CallReport Units", () => {
     
     it("Should create and return a new document", () => {
       return CallReport.createDoc(fixture)
-        .then((doc) => {
+        .then(doc => {
           should.exist(doc);
           doc.should.not.be.Array();
           doc.callTime.should.be.a.Date();
@@ -36,7 +36,7 @@ describe("CallReport Units", () => {
           doc.username.should.equal("TEST001");
           doc.phoneNumber.should.be.a.String();
         });
-    }); /* End of 'it' #createDoc create new doc */
+    });
   }); /* End of 'describe' #createDoc */
   
   describe("#fetch()", () => {
@@ -46,20 +46,20 @@ describe("CallReport Units", () => {
       return CallReport.remove({})
         .then(() => {
           return CallReport.createDoc(fixture);
-        }).then((doc) => {
+        }).then(doc => {
           id = doc._id;
       });
-    }); /* End of 'before' #fetch() */
+    });
     
     it("Should fetch one document", () => {
       return CallReport.fetch(id)
-        .then((doc) => {
+        .then(doc => {
           doc.should.have.property("_id");
           doc.should.have.property("customer");
           doc.phoneNumber.should.be.a.String();
           doc.title.should.be.equal("TestTitle");
         });
-    }); /* End of 'it' #fetch() */
+    });
   }); /* End of 'describe' #fetch() */
   
   describe('#list()', () =>{
@@ -99,7 +99,7 @@ describe("CallReport Units", () => {
             
             return _.flatten([phoneDocs, userDocs, activityDocs, dateDocs]);
           })
-          .then((docs) => {
+          .then(docs => {
             return CallReport.insertMany(docs);
           })
           .then(resolve)
@@ -115,34 +115,34 @@ describe("CallReport Units", () => {
       };
       
       return CallReport.list(options)
-        .then((docs) => {
+        .then(docs => {
           docs.should.be.an.Array();
           docs.should.have.length(25);
           options.skip += 25;
           
           return CallReport.list(options);
         })
-        .then((docs) => {
+        .then(docs => {
           docs.should.be.an.Array();
           docs.should.have.length(25);
           options.skip += 25;
           
           return CallReport.list(options);
         })
-        .then((docs) => {
+        .then(docs => {
           docs.should.be.an.Array();
           docs.should.have.length(25);
           options.skip += 25;
           
           return CallReport.list(options);
         })
-        .then((docs) => {
+        .then(docs => {
           docs.should.be.an.Array();
           docs.should.have.length(25);
           
           return null;
         });
-    }).slow(500); /* End of 'it' list 4 pages #list() */
+    }).slow(500);
     
     it("Should list callreports with specific callTime", () => {
       var options = {
@@ -154,17 +154,17 @@ describe("CallReport Units", () => {
       };
       
       return CallReport.list(options)
-        .then((docs) => {
+        .then(docs => {
            docs.should.be.an.Array();
            docs.should.be.length(25);
            
-           docs.forEach((doc) => {
+           docs.forEach(doc => {
              doc.callTime.should.be.a.Date();
              doc.callTime.should.eql(new Date('Wed Jan 18 2017 11:32:45 GMT-0600 (CST)'));
            });
         });
       
-    }); /* End of 'it' list specific callTime #list() */
+    });
     
   }); /* End of 'describe' #list() */
   
--- test/unit/partOrder.js
+++ test/unit/partOrder.js
@@ -18,11 +18,11 @@ const fixture     = require('../fixture/partOrder.json');
 const userFixture = require('../fixture/user.json');
 const partFixture = require('../fixture/part.json');
 
-before((done) => {
+before(done => {
   PartOrder.remove({}, done);
 });
 
-after((done) => {
+after(done => {
   PartOrder.remove({}, done);
 });
 
@@ -47,56 +47,138 @@ describe("PartOrder Units", () => {
         partId = part._id;
         partDoc = part;
       });
-  }); /* End of 'describe' 'before' PartOrder Unit Tests */
+  });
   
   after(() => {
     return User.remove({})
       .then(() => {
         return Part.remove({});
       });
-  }); /* End of 'describe' 'after' PartOrder Unit Tests */
+  });
   
   describe("#createDoc()", () => {
     let manualPartFixture = _.cloneDeep(fixture);
     
     it("Should create a non manual partOrder document", () => {
       return PartOrder.createDoc(fixture)
-        .then((doc) => {
+        .then(doc => {
           should.exist(doc);
           doc.should.not.be.Array();
           doc.timeCreated.should.be.a.Date();
           should.exist(doc.timeSubmitted);
           doc.timeSubmitted.should.be.a.Date();
           doc.should.have.property("_id");
+          should.exist(doc.orderId);
+          doc.orderId.should.be.a.String();
           
           doc.part.should.be.an.instanceOf(Object);
           doc.part.should.have.property('isManual');
           doc.part.MPN.should.equal('111000/TEST');
         });
-    }); /* End of 'it' #createDoc() create new non manual doc */
+    });
   
     it("Should create a manual partOrder document", () => {
       manualPartFixture.partNSID = 0;
       manualPartFixture.part.isManual = true;
       
       return PartOrder.createDoc(manualPartFixture)
-        .then((doc) => {
+        .then(doc => {
           should.exist(doc);
           doc.should.not.be.Array();
           doc.timeCreated.should.be.a.Date();
           should.exist(doc.timeSubmitted);
           doc.timeSubmitted.should.be.a.Date();
           doc.should.have.property("_id");
-
+          should.exist(doc.orderId);
+          doc.orderId.should.be.a.String();
+          
           doc.part.should.be.an.instanceOf(Object);
           doc.part.should.have.property('isManual');
           doc.part.MPN.should.equal('111-000');
           doc.part.isManual.should.equal(true);
         });
-    }); /* End of 'it' #createDoc() create new manual doc */
+    });
   }); /* End of 'describe' #createDoc() */
   
   describe("#updateDoc()", () => {
+    let orderId, updated_at, updatingDoc;
+    
+    beforeEach(() => {
+      return PartOrder.remove({})
+        .then(() => {
+          return PartOrder.createDoc(fixture);
+        })
+        .then(doc => {
+          updatingDoc = _.cloneDeep(doc);
+          updatingDoc.trackingNumber = '1234-5678-910';
+          updatingDoc.carrier = 'UPS';
+          updatingDoc.approvedBy = 'TEST001';
+          orderId = doc.orderId;
+          updated_at = doc.updated_at;
+        })
+    });
+    
+    afterEach(() => {
+      return PartOrder.remove({});
+    });
+    
+    it("Should set timeShipped on status change to 'shipped'", () => {
+      updatingDoc.status = 'shipped';
+      
+      return PartOrder.updateDoc(orderId, updatingDoc)
+        .then(doc => {
+          should.exist(doc);
+          doc.should.not.be.Array();
+          doc.timeShipped.should.be.a.Date();
+          
+          doc.updated_at.should.not.eql(updated_at);
+          doc.trackingNumber.should.be.equal('1234-5678-910');
+        });
+    });
+    
+    it("Should set many fields on status change to 'completed'", () => {
+      updatingDoc.status = 'shipped';
+      
+      return PartOrder.updateDoc(orderId, updatingDoc)
+        .then(doc => {
+          doc.status = 'completed';
+          doc.completedBy = 'TEST002';
+          updated_at = doc.updated_at;
+          return PartOrder.updateDoc(orderId, doc);
+        })
+        .then(doc => {
+          should.exist(doc);
+          doc.timeComplete.should.be.a.Date();
+          
+          doc.updated_at.should.not.eql(updated_at);
+          doc.completedBy.should.be.equal('TEST002');
+          doc.carrier.should.be.equal('UPS');
+          doc.done.should.be.equal(true);
+        })
+    });
+  
+    it("Should set many fields on status change to 'canceled'", () => {
+      updatingDoc.status = 'shipped';
+    
+      return PartOrder.updateDoc(orderId, updatingDoc)
+        .then(doc => {
+          doc.status = 'canceled';
+          doc.completedBy = 'TEST002';
+          doc.comment = 'TestCancel';
+          doc.source = 'Orion';
+          updated_at = doc.updated_at;
+          return PartOrder.updateDoc(orderId, doc);
+        })
+        .then(doc => {
+          should.exist(doc);
+          doc.timeComplete.should.be.a.Date();
+          
+          doc.updated_at.should.not.eql(updated_at);
+          doc.comment.should.be.equal('TestCancel');
+          doc.approvedBy.should.be.equal('TEST001');
+          doc.done.should.be.equal(true);
+        })
+    });
     
   }); /* End of 'describe' #updateDoc() */
   
