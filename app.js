f73e88631f51e3bab2002c1ec93e7607bfb7a041
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-05-08 11:16:22 -0500

    ORION-40 Add CORS Support fix access control

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ const app = express();
 //CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS");
   next();
 });
 
