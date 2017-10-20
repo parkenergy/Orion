aef6f711276c5b6bbefca62ff4cea46286e707a4
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-10-20 11:20:40 -0500

    Fix search for parts in part orders Possible fix for auto syncing to netsuite. running tests now.

--- test/unit/testCreateSyncNewWO.js
+++ test/unit/testCreateSyncNewWO.js
@@ -53,7 +53,6 @@ describe("Workorder Separate important unit", () => {
   
   describe("#createDoc()", () => {
     it('should create doc and unit info should match', () => {
-      console.log(2)
       return WorkOrder.createDoc(workorder)
         .then((doc) => {
           should.exist(doc);
