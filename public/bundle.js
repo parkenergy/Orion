054a61fdee6c7be1197a107a394770fc1d60e060
rransom75 <84739625+rransom75@users.noreply.github.com>
2021-11-08 09:55:52 -0600

    Update bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -650,7 +650,7 @@ angular.module("CommonControllers").controller("LoginCtrl", [
     Users,
     $auth
   ) {
-    ($scope.hideLocalLogin = true),
+    ($scope.hideLocalLogin = false),
       ($scope.title = "Login"),
       ($scope.message = "Use your local login to access the system."),
       ($scope.returnUrl = $routeParams.returnUrl),
@@ -701,7 +701,7 @@ angular.module("CommonControllers").controller("LoginCtrl", [
             });
       }),
       ($scope.showLocalLogin = function () {
-        $scope.hideLocalLogin = true;
+        $scope.hideLocalLogin = false;
       });
   },
 ]);
