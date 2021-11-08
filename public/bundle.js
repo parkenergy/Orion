8d7387fd257513dff14fa7c8cbd5ee31f032728a
rransom75 <84739625+rransom75@users.noreply.github.com>
2021-11-08 12:47:16 -0600

    Update bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -663,7 +663,7 @@ angular.module("CommonControllers").controller("LoginCtrl", [
         ),
       ($scope.localLogin = function () {
         console.log("localLogin"),
-            alert('test');
+            console.log(JSON.sgringify(Users); 
           ($scope.username = document.getElementById("username").value);
         ($scope.username = $scope.username.toUpperCase()),
           console.log($scope.username),
